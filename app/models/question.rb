1527cdd6c737d46033cceb14abec9b8a7be7575b
lucaszanela <lucasf.zanela@gmail.com>
2018-08-02 16:47:32 -0300

    Alterações de design 02-08

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
-  has_many :alternatives
+  has_many :alternatives, dependent: :destroy
   accepts_nested_attributes_for :alternatives, reject_if: :all_blank, allow_destroy: true
 
   validates :description, presence:true
