aa6bcf9ed38b7ab208c5b694f8dc655c3983e9f1
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2018-09-13 15:09:00 -0300

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
+  has_many :answers
 
   def set_default_role
     self.role ||= :user
